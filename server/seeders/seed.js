6b621315016f419c148303d3db24022ffc80a3e1
MeerKar <meku.jithu@gmail.com>
2024-05-22 21:20:06 -0700

    adding seeds

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,24 +1,61 @@
-const db = require('../config/connection');
-const { User, Thought } = require('../models');
-const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
-const cleanDB = require('./cleanDB');
+const db = require("../config/connection");
+const { User, Book, Review, Club } = require("../models");
+const userSeeds = require("./userSeeds.json");
+const bookSeeds = require("./bookSeeds.json");
+const reviewSeeds = require("./reviewSeeds.json");
+const clubSeeds = require("./clubSeeds.json");
+const cleanDB = require("./cleanDB");
 
-db.once('open', async () => {
+db.once("open", async () => {
   try {
-    await cleanDB('Thought', 'thoughts');
-
-    await cleanDB('User', 'users');
+    await cleanDB("Book", "books");
+    await cleanDB("User", "users");
+    await cleanDB("Review", "reviews");
+    await cleanDB("Club", "clubs");
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
+    for (let i = 0; i < bookSeeds.length; i++) {
+      const { _id, author } = await Book.create(bookSeeds[i]);
+      await User.findOneAndUpdate(
+        { username: author },
         {
           $addToSet: {
-            thoughts: _id,
+            books: _id,
+          },
+        }
+      );
+    }
+
+    for (let i = 0; i < reviewSeeds.length; i++) {
+      const { _id, authorId, bookId } = await Review.create(reviewSeeds[i]);
+      await User.findByIdAndUpdate(authorId, {
+        $addToSet: {
+          reviews: _id,
+        },
+      });
+      await Book.findByIdAndUpdate(bookId, {
+        $addToSet: {
+          reviews: _id,
+        },
+      });
+    }
+
+    for (let i = 0; i < clubSeeds.length; i++) {
+      const { _id, books, users } = await Club.create(clubSeeds[i]);
+      await Book.updateMany(
+        { _id: { $in: books } },
+        {
+          $addToSet: {
+            clubs: _id,
+          },
+        }
+      );
+      await User.updateMany(
+        { _id: { $in: users } },
+        {
+          $addToSet: {
+            clubs: _id,
           },
         }
       );
@@ -28,6 +65,6 @@ db.once('open', async () => {
     process.exit(1);
   }
 
-  console.log('all done!');
+  console.log("all done!");
   process.exit(0);
 });
